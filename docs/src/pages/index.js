16b3a60a863368bcee386fbee3fd265dffd4531b
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-11-10 23:28:20 +0530

    Updated the content in the home

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import Sidebar from '../components/sidebar';
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-    </div>
+    <h1>Space</h1>
+    <p>Space is a minimal react ui library</p>
     <Sidebar />
   </Layout>
 )
