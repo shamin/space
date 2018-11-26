fd614ee1d68938df8d1b6769f9d4bbabc829e487
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-11-26 19:21:51 +0530

    Added a loader component

---
+++ src/loaders/stories/index.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Loader } from '..'
+
+storiesOf('Loader', module)
+  .add('simple loader', () => (
+    <Loader />
+  ))
