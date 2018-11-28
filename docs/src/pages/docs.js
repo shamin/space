f28b981c91b63e5131ef5cbd1184a87b7121f22f
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-11-28 23:37:28 +0530

    Created a home page for space

---
+++ docs/src/pages/docs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Space Documentation</h1>
+    <p>Space is a minimal react ui library</p>
+  </Layout>
+)
+
+export default IndexPage
--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
+const Container = styled.div`
+  padding: 40vh 0;
+  background: #f1f2f6;
+  height: 100vh;
+  text-align: center;
+`
+
+const Button = styled.a`
+  background: transparent;
+  padding: 7px 20px;
+  border: 1px solid #5f27cd;
+  display: inline;
+  text-decoration: none;
+  color: #5f27cd;
+  transition: 0.5s ease;
+  &:hover{
+    background: #5f27cd;
+    color: #f1f2f6;
+  }
+`
 
 const IndexPage = () => (
-  <Layout>
-    <h1>Space Documentation</h1>
-    <p>Space is a minimal react ui library</p>
-  </Layout>
+  <Container>
+    <h1>Space</h1>
+    <p>Minimal react ui library</p>
+      <Button href="/docs">Documentation</Button>
+  </Container>
 )
 
 export default IndexPage
