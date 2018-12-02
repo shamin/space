c434ff294f4356394b5abb9f2537a80b50af6086
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-12-02 23:34:29 +0530

    Added links to pagination component

--- src/pagination/pagination.js
+++ src/pagination/pagination.js
@@ -2,16 +2,27 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const PaginationLinks = styled.ul`
-
+  list-style: none;
+  li {
+    display: inline;
+    padding: 10px;
+    &.active {
+      background: #5f27cd;
+      color: #ffffff;
+    }
+  }
 `;
 
-export class Pagination extends React.Component{
-  render(){
-    return(
-      <div>
-        Pages
-      </div>
-    )
+export class Pagination extends React.Component {
+  render() {
+    return (
+      <PaginationLinks>
+        {this.props.pages.map(page => (
+          <li className={page === this.props.currentpage ? "active" : ""}>
+            {page}
+          </li>
+        ))}
+      </PaginationLinks>
+    );
   }
 }
-
