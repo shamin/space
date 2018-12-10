aa66e179a934b9b640faab3cc19fa7aca5366907
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-12-10 23:47:44 +0530

    Added some global styles to home page

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 import { Link } from 'gatsby'
 import { Button } from '../../../src'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    background: #f1f2f6;
+  }
+`
 
 const Container = styled.div`
   font-family: monospace;
   padding: 40vh 0;
-  background: #f1f2f6;
-  max-height: 100vh;
   text-align: center;
 `
 
 const IndexPage = () => (
   <Container>
+    <GlobalStyle />
     <h1>Space</h1>
     <p>Minimal react ui library</p>
       <Link to="/docs"><Button>Documentation</Button></Link>
